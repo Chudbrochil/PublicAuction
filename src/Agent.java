c328660216fd71ed9db44a797e6d4c8e93d6ccf4
Jacob Collins <jacobacollins123@gmail.com>
2017-11-27 17:07:29 -0700

    Add files via upload

--- src/Agent.java
+++ src/Agent.java
@@ -1,6 +1,8 @@
+import java.io.*;
+import java.net.Socket;
 import java.util.ArrayList;
 
-public class Agent
+public class Agent implements Serializable
 {
 
 
@@ -9,10 +11,14 @@ public class Agent
     ArrayList<AuctionHouse> auctionHouses;
 
     private int accountNum;
-    private String bankKey;
+    private String bankKey, name;
     private String biddingKey;
     private double accountBalance;
 
+
+public Agent(String name){
+    this.name = name;
+}
 
     // Returns true if bid is successful
     private boolean placeBid()
@@ -27,6 +33,10 @@ public class Agent
 
     }
 
+    public void setBankKey(String bankKey){
+        this.bankKey = bankKey;
+    }
+
     // This gets called on auction central to get the latest list of auctionhouses;
     private void getUpdatedAHList()
     {
@@ -38,5 +48,27 @@ public class Agent
     {
 
     }
+
+    public String getName(){
+    return this.name;
+    }
+
+    public void setAccountNum(int accountNum){
+    this.accountNum = accountNum;
+    }
+
+    public int getAccountNum(){
+        return accountNum;
+    }
+
+
+    public void setAccountBalance(int num){
+        this.accountBalance = num;
+    }
+
+    public double getAccountBalance(){
+        return accountBalance;
+    }
+
 
 }
