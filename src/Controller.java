70cc8a2b4faf5573202774de23fb3f4dbed57cba
Anthony Galczak <wgalczak@gmail.com>
2017-11-28 18:47:14 -0700

    Small UI additions, placeBid button, userOutput field

--- src/Controller.java
+++ src/Controller.java
@@ -1,12 +1,48 @@
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 public class Controller
 {
 
     @FXML
+    private TextField tfUserOutput;
+
+    @FXML
     private void initialize()
     {
-        AuctionHouse ah = new AuctionHouse();
+
+
+
+
+
+
+
+
+
+        // TODO: Only for testing, delete as implementation continues
+        //AuctionHouse ah = new AuctionHouse();
     }
+
+
+    /**
+     * User wants to place a bid
+     */
+    @FXML
+    private void placeBid()
+    {
+        tfUserOutput.setText("User pressed placeBid button.");
+    }
+
+    /**
+     * TODO: UI elements needed...
+     *
+     * 1. A list of available items from the auction houses... OPTIONAL: status next to each item, currentBid etc.
+     * 2. A way to select an item... at the start this could be a field that takes an int index.
+     *
+     */
+
+
+
+
 
 }
