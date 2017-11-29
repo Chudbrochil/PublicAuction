1e3c13787277db37cf0f092626c544231ff15b58
careya42 <annacarey42@gmail.com>
2017-11-28 20:23:43 -0700

    Added Message and MessageType

---
+++ src/Message.java
@@ -0,0 +1,12 @@
+/**
+ * This is the object that can be sent between classes, storing information.
+ */
+public class Message
+{
+    MessageType type;
+    
+    public Message()
+    {
+    
+    }
+}
---
+++ src/MessageType.java
@@ -0,0 +1,37 @@
+/**
+ * If you create a Message, assign it a type. Those who receive messages will respond based on the MessageType.
+ *
+ *
+ */
+
+public enum MessageType
+{
+    /**
+     * Agent to AuctionHouse:
+     *      "Hey, what items do you have?" (Establish socket communication.)
+     *      "I want to place a bid on Item ____. This is my Bidding Key."
+     *      "I am no longer interested in your items. Goodbye!" //closes.
+     *
+     * AuctionHouse to Agent:
+     *      (Response) "These are my items and their minimum bids!"
+     *      (Response) Accept, Reject (Item, amount)
+     *      Pass, Winner (Item, amount)
+     *      "Hey, I'm going out of business." //closes. CLOSE_SOCKET_REQUEST
+     *
+     * AuctionHouse to AutcionCentral
+     *      "Please place a hold on BidderID account for amount" //PLACE_HOLD, null boolean, BidderID, amount
+     *      "I'm shutting down!" CLOSE_SOCKET_REQUEST //Every time AuctionCentral gets one of these, it pings agents? to say "These houses closed!"
+     *                                                  //but it doesn't have a reference... :(
+     * AuctionCentral to Bank
+     *      "Please place a hold on BankKey account for amount." //PLACE_HOLD, null boolean, BankKey, amount
+     *
+     * Bank to AuctionCentral
+     *      "I placed a hold on their account." //PLACE_HOLD, true boolean, BankKey, amount
+     *      "I cannot place said hold." //PLACE_HOLD, false boolean, BankKey, amount
+     *
+     * AuctionCentral to AuctionHouse
+     *      "I placed a hold on their account." //PLACE_HOLD, true boolean, BidderID, amount
+     *      "I cannot place said hold." //PLACE_HOLD, false boolean, BidderID, amount
+     *
+     */
+}
