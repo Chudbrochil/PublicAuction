a01a9c4f99ed2a8e98c647cbee1005ae1e5368a0
Anthony Galczak <wgalczak@gmail.com>
2017-12-01 17:01:35 -0700

    Added some data structures to AuctionCentral, most specifically the one for the list of auctionhouses

--- src/AuctionCentral.java
+++ src/AuctionCentral.java
@@ -1,15 +1,16 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
 
 public class AuctionCentral
 {
-
-
-    HashMap<AuctionHouse, String> map = new HashMap<AuctionHouse, String>();
-    Random random = new Random();
+    private HashMap<String, String> biddingKeyToBankKey;
+    private ArrayList<AuctionHouse> listOfAHs;
 
     public AuctionCentral()
     {
+        biddingKeyToBankKey = new HashMap<>();
+        listOfAHs = new ArrayList<>();
 
     }
 
@@ -21,14 +22,11 @@ public class AuctionCentral
         // TODO: If you try to register an agent with AC before bank, this might throw an error
         agent.setBiddingKey(Bank.getKey(agent.getBankKey() + agent.getName()));
         System.out.println("Bidding key: " + agent.getBiddingKey() + "\n");
-
-
     }
 
 
     public void registerAuctionHouse(AuctionHouse ah)
     {
-        map.put(ah, ah.getName());
         ah.setIDs("public Id", "Secret key");
         System.out.println("Auction house is registered...");
         //todo: implement. See Bank.registerAgent(Agent agent)
@@ -42,9 +40,6 @@ public class AuctionCentral
     }
 
 
-    public HashMap<AuctionHouse, String> getMap()
-    {
-        return map;
-    }
+    public ArrayList<AuctionHouse> getListOfAHs() { return listOfAHs; }
 
 }
